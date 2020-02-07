e989d5dbc702f6c65f6333ba5b2d035f76fcccbc
Lonely-Person <528435426@qq.com>
2020-02-08 00:43:39 +0800

    静态内部类也有着一个致命的缺点，就是传参的问题

--- singleton/lazy/InnerClassSingleton.java
+++ singleton/lazy/InnerClassSingleton.java
@@ -11,6 +11,7 @@ public class InnerClassSingleton {
 
     //静态内部类必须是private修饰的，如果不是private的，new InnerClassSingleton.InnerClass();
     //这种使用静态内部类的方法跟类的加载机制有关
+    //静态内部类也有着一个致命的缺点，就是传参的问题
     private static class InnerClass{
         private static final InnerClassSingleton instance = new InnerClassSingleton();
     }
